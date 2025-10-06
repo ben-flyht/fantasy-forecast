86380538a22816cd53413854a571bf336e36929e
Ben Morrison <ben@flyht.uk>
2025-10-06 16:42:29 +0100

    Add select link

--- app/components/player_card_component.rb
+++ app/components/player_card_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class PlayerCardComponent < ViewComponent::Base
-  def initialize(player: nil, show_position: false)
+  def initialize(player: nil, show_position: false, selection_url: nil)
     @player = player
     @show_position = show_position
+    @selection_url = selection_url
   end
 end
