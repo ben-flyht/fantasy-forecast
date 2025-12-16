792c5975197ae5d3509e06aa494c10287baf14e7
Ben Morrison <ben@flyht.uk>
2025-12-16 17:08:20 +0000

    Make default availability be 100

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_16_141311) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_16_170720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_stat_statements"
@@ -88,7 +88,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_16_141311) do
     t.bigint "team_id"
     t.integer "code"
     t.string "status"
-    t.integer "chance_of_playing"
+    t.integer "chance_of_playing", default: 100
     t.index ["fpl_id"], name: "index_players_on_fpl_id", unique: true
     t.index ["position", "team_id"], name: "index_players_on_position_and_team_id"
     t.index ["team_id"], name: "index_players_on_team_id"
